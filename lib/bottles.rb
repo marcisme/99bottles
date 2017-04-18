476c6307baa9f3bb49081b4eaaa77b00c3c8b13f
Marc Schwieterman <marc@marcisme.com>
2017-04-17 20:30:16 -0700

    Clean up strings

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -8,22 +8,27 @@ class Bottles
   end
 
   def verse(num_bottles)
-    if num_bottles > 2
-"""#{num_bottles} bottles of beer on the wall, #{num_bottles} bottles of beer.
-Take one down and pass it around, #{num_bottles - 1} bottles of beer on the wall.
-"""
-    elsif num_bottles > 1
-"""#{num_bottles} bottles of beer on the wall, #{num_bottles} bottles of beer.
-Take one down and pass it around, #{num_bottles - 1} bottle of beer on the wall.
-"""
-    elsif num_bottles > 0
-"""1 bottle of beer on the wall, 1 bottle of beer.
-Take it down and pass it around, no more bottles of beer on the wall.
-"""
+    case num_bottles
+    when 0
+      <<~EOS
+        No more bottles of beer on the wall, no more bottles of beer.
+        Go to the store and buy some more, 99 bottles of beer on the wall.
+      EOS
+    when 1
+      <<~EOS
+        1 bottle of beer on the wall, 1 bottle of beer.
+        Take it down and pass it around, no more bottles of beer on the wall.
+      EOS
+    when 2
+      <<~EOS
+        #{num_bottles} bottles of beer on the wall, #{num_bottles} bottles of beer.
+        Take one down and pass it around, #{num_bottles - 1} bottle of beer on the wall.
+      EOS
     else
-"""No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.
-"""
+      <<~EOS
+        #{num_bottles} bottles of beer on the wall, #{num_bottles} bottles of beer.
+        Take one down and pass it around, #{num_bottles - 1} bottles of beer on the wall.
+      EOS
     end
   end
 end
